7be7d9ba6e9fed5f25813c5b46ef3a6f83cb8a4b
Vikram <vikram@nimesa.io>
2023-08-25 13:29:02 +0530

    Commit 2

--- src/Test.java
+++ src/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello Commit 1");
         System.out.println("Commit From Remote Origin");
+        System.out.println("Hello Commit 2");
     }
 }
